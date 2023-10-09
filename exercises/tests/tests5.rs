e5fab66a921353ed3e30c843bf1ef33b17f58629
ASUKA39 <573470041@qq.com>
2023-10-10 03:28:14 +0800

    update

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -32,7 +32,8 @@ unsafe fn modify_by_address(address: usize) {
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
     unsafe {
-        todo!("Your code goes here")
+        let ptr = address as *mut u32;
+        *ptr = 0xAABBCCDD;
     }
 }
 
